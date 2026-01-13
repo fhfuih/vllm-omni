ab8473e5a3b7dd0030b9d1b3225a8d4c8e51b614
Huang, Zeyu <11222265+fhfuih@users.noreply.github.com>
2026-01-13 07:11:02 +0000

    update qwen image pipeline

--- vllm_omni/diffusion/models/qwen_image/pipeline_qwen_image.py
+++ vllm_omni/diffusion/models/qwen_image/pipeline_qwen_image.py
@@ -667,7 +667,7 @@ class QwenImagePipeline(
         num_inference_steps = req.sampling_params.num_inference_steps or num_inference_steps
         generator = req.sampling_params.generator or generator
         true_cfg_scale = req.sampling_params.true_cfg_scale or true_cfg_scale
-        req_num_outputs = getattr(req, "num_outputs_per_prompt", None)
+        req_num_outputs = req.sampling_params.num_outputs_per_prompt or num_images_per_prompt
         if req_num_outputs and req_num_outputs > 0:
             num_images_per_prompt = req_num_outputs
         # 1. check inputs
