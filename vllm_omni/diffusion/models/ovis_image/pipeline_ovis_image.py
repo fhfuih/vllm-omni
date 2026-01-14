2e33eee59dcf6651ccd5baabf033f165c666502d
Huang, Zeyu <11222265+fhfuih@users.noreply.github.com>
2026-01-14 07:22:25 +0000

    update ovis image pipelines
    Signed-off-by: Huang, Zeyu <11222265+fhfuih@users.noreply.github.com>

--- vllm_omni/diffusion/models/ovis_image/pipeline_ovis_image.py
+++ vllm_omni/diffusion/models/ovis_image/pipeline_ovis_image.py
@@ -520,8 +520,8 @@ class OvisImagePipeline(
     def forward(
         self,
         req: OmniDiffusionRequest,
-        prompt: str | list[str] = None,
-        negative_prompt: str | list[str] = None,
+        prompt: str | list[str] | None = None,
+        negative_prompt: str | list[str] | None = None,
         guidance_scale: float = 5.0,
         height: int | None = None,
         width: int | None = None,
@@ -607,16 +607,23 @@ class OvisImagePipeline(
             [`~pipelines.ovis_image.OvisImagePipelineOutput`] if `return_dict` is True, otherwise a `tuple`. When
             returning a tuple, the first element is a list with the generated images.
         """
-        prompt = req.prompt if req.prompt is not None else prompt
-        negative_prompt = req.negative_prompt if req.negative_prompt is not None else negative_prompt
-        height = req.height or self.default_sample_size * self.vae_scale_factor
-        width = req.width or self.default_sample_size * self.vae_scale_factor
-        num_inference_steps = req.num_inference_steps or num_inference_steps
-        guidance_scale = req.guidance_scale if req.guidance_scale is not None else guidance_scale
-        generator = req.generator or generator
-        req_num_outputs = getattr(req, "num_outputs_per_prompt", None)
-        if req_num_outputs and req_num_outputs > 0:
-            num_images_per_prompt = req_num_outputs
+        prompt = [p if isinstance(p, str) else p.get("prompt", "") for p in req.prompts] or prompt
+        negative_prompt = [
+            "" if isinstance(p, str) else p.get("negative_prompt", "") for p in req.prompts
+        ] or negative_prompt
+        height = req.sampling_params.height or self.default_sample_size * self.vae_scale_factor
+        width = req.sampling_params.width or self.default_sample_size * self.vae_scale_factor
+        num_inference_steps = req.sampling_params.num_inference_steps or num_inference_steps
+        sigmas = req.sampling_params.sigmas or sigmas
+        guidance_scale = (
+            req.sampling_params.guidance_scale if req.sampling_params.guidance_scale is not None else guidance_scale
+        )
+        generator = req.sampling_params.generator or generator
+        num_images_per_prompt = (
+            req.sampling_params.num_outputs_per_prompt
+            if req.sampling_params.num_outputs_per_prompt > 0
+            else num_images_per_prompt
+        )
 
         # Steps:
         # 1. Check Inputs
